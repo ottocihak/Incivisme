4aa62bab71662e9a104f81d02b3db95a362b1c81
otto <44638093@iespoblenou.org>
2022-01-24 14:30:54 +0100

    commit

--- app/src/main/java/com/example/incivisme/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/incivisme/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,14 @@
 package com.example.incivisme.ui.notifications;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +30,10 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public class NotificationsFragment extends Fragment {
@@ -71,18 +80,75 @@ public class NotificationsFragment extends Fragment {
                     location -> {
                         if (location != null) {
                             Log.d("-----------------------", "getLocation: ");
-                            mLastLocation = location;
-                            locationTextView.setText(
-                                    getString(R.string.location_text,
-                                            mLastLocation.getLatitude(),
-                                            mLastLocation.getLongitude(),
-                                            mLastLocation.getTime()));
+                            new FetchAddressTask(getContext()).execute(location);
+                            locationTextView.setText(getString(R.string.address_text,
+                                    "Loading...",
+                                    System.currentTimeMillis()));
                         } else {
                             locationTextView.setText("No location known");
                         }
                     });
         }
     }
+
+    private class FetchAddressTask extends AsyncTask<Location, Void, String> {
+        private final String TAG = FetchAddressTask.class.getSimpleName();
+        private Context myAppContext;
+
+        FetchAddressTask(Context applicationContext) {
+            myAppContext = applicationContext;
+        }
+
+        @Override
+        protected String doInBackground(Location... locations) {
+            Geocoder geocoder = new Geocoder(myAppContext,
+                    Locale.getDefault());
+            Location location = locations[0];
+            List<Address> addresses = null;
+            String resultMessage = "";
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        1);
+            }catch (IOException ioException) {
+                resultMessage = "service not available";
+                Log.e(TAG, resultMessage, ioException);
+            }catch (IllegalArgumentException illegalArgumentException) {
+                resultMessage = "Coordinates not available";
+                Log.e(TAG, resultMessage + "." +
+                        "\nLatitude = " + location.getLatitude() +
+                        "\nLongitude = " + location.getLongitude(),
+                        illegalArgumentException);
+            }
+
+            if (addresses == null || addresses.size() == 0) {
+                if (resultMessage.isEmpty()) {
+                    resultMessage = "Address not found";
+                    Log.e(TAG, resultMessage);
+                }
+            }else {
+                Address address = addresses.get(0);
+                ArrayList<String> addressParts = new ArrayList<>();
+
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    addressParts.add(address.getAddressLine(i));
+                }
+
+                resultMessage = TextUtils.join("\n", addressParts);
+            }
+
+            return resultMessage;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            locationTextView.setText(getString(R.string.address_text,
+                    s, System.currentTimeMillis()));
+        }
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
